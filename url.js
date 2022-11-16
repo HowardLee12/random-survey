66ac24f6da45c0e49e18281b2777e212da8e9f50
HowardLee12 <114634129+HowardLee12@users.noreply.github.com>
2022-11-16 18:07:16 +0800

    Update url.js

--- url.js
+++ url.js
@@ -8,7 +8,7 @@
 
 
 const urls = [
-	'https://hhming.moe',
-	'https://github.com/hms5232',
-	'https://gitlab.com/hms5232'
+	' https://forms.gle/BTYipaNwXrpHmMPb8',
+	'https://forms.gle/sti8RwqurwJfgzZD9',
+	' https://forms.gle/NxRbTvj46vBkCTgk6'
 ];
